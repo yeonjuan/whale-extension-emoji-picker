8073e35c01c2f54bd4209710f6e9f0dd2697adcd
yeonjuan <yeonjuan93@naver.com>
2019-10-03 23:45:23 +0900

    chore: change directory structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 class App extends React.Component {
   public render() {
     return (
-      <div className="App"/>
+      <div className="App">
+        Hello World
+      </div>
     );
   }
 }
