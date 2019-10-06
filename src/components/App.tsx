27e69762dcf75121747241ae63b8a1c4fffa2e17
yeonjuan <yeonjuan93@naver.com>
2019-10-06 18:03:09 +0900

    feat: add components on app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react';
-import './App.css';
+import '../css/App.css';
+import EmojiPicker from './EmojiPicker';
+import {Title} from './Title';
+import Toast from './Toast';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        Hello World
-      </div>
-    );
-  }
-}
+const App: React.FunctionComponent = () => {
+  const [toast, setToast] = React.useState({
+    message: '',
+    show: false,
+  });
+  const handleClosed = () => setToast({
+    message: '',
+    show: false,
+  });
+  const handlePick = (emoji: string) => setToast({
+    message: `${emoji} Copied!`,
+    show: true,
+  });
+
+  const {show, message} = toast;
+  return (
+    <div className="App">
+      <Title title="Emoji Picker"/>
+      <EmojiPicker onPick={handlePick}/>
+      <Toast show={show} message={message} onClosed={handleClosed}/>
+    </div>
+  )
+};
 
 export default App;
