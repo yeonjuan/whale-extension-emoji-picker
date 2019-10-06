764b70b8393d9d249945a38e38c6d7a6057fa58e
yeonjuan <yeonjuan93@naver.com>
2019-10-06 17:53:46 +0900

    feat: add Toast component

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+export interface IToastProps {
+  show: boolean,
+  onClosed?: () => void;
+  message: string;
+}
+
+const Toast: React.SFC<IToastProps> = ({
+  show = false,
+  onClosed,
+  message,
+}: IToastProps) => {
+
+  const [visible, setVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    if (show !== visible) {
+      setVisible(show);
+    }
+  }, [show]);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+      if (onClosed) {
+        onClosed();
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [show]);
+
+  return (
+    <div
+      id={'toast'}
+      className={visible ? "show": ""}>
+      {message}
+    </div>
+  )
+};
+
+export default Toast;
